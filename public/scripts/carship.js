e2770c142af2b90e06a25281ba7b980988987e8c
chri-roch12 <chris.prochnow@gmail.com>
2017-02-18 22:56:25 -0800

    Added start of carShip POST. No errors, but not adding entry to DB. reworked GET all.

--- public/scripts/carship.js
+++ public/scripts/carship.js
@@ -1,40 +1,54 @@
+
+
+
 $(document).ready(function() {
 
   $.ajax({
 		method: 'GET',
-		url: '/api/carShip',
+		url: "/api/carShip",
 		success: onSuccessCars,
     error: errorCars
 	});
 
-  // $.ajax({
-	// 	method: 'GET',
-	// 	url: '/api/carShip/:id',
-	// 	success: onSuccessOneCar,
-  //   error: errorOneCar
-	// });
+  $("#singlebutton").on("submit", function(event) {
+    event.preventDefault();
+    var formData = $(this).serialize();
+    debugger;
+    console.log("formData", formData);
+    $.post("api/carShip/", formData, function(carShip) {
+      console.log("carShip after POST", carShip);
+      rendercarShip(carShip);
+    });
+    $(this).trigger("reset");
+  });
 
+//end of ready
+})
 
-
-// ajax above, functions below
-
-function onSuccessCars(carResponse) {
-  carResponse.forEach(function(value, index) {
-    $("#carList").append(`
-      <li>carShip Name: ${value.name} | carShip Color: ${value.color} | carShip Speed: ${value.speedValue}
-      </li>
-      `);
+//All carShips
+function onSuccessCars(carResponses) {
+  carResponses.forEach(function(carResponse) {
+    rendercarShip(carShip);
   });
 }
 
 function errorCars(e) {
-  console.log('uh oh');
+  console.log('error on all carShip call');
   $('#carList').text('Failed to load carShip');
 }
 
-function onSuccessOneCar() {
-
+//create new carShip
+function NewCarSuccess(json) {
+  $("#newcarShipForm input").val("");
 }
 
-//end of ready
-})
+
+function rendercarShip(carShip) {
+  console.log("carShip html", carShip)
+  var carShipHtml = (`
+    <li>carShip Name: ${value.name} | carShip Color: ${value.color} | carShip Speed: ${value.speedValue} |
+       <button type="button" name="button" class="deleteBtn btn btn-danger pull-right" data-id="${this._id}">Delete</button>
+    </li>
+    `);
+  $("#carList").append(carShipHtml);
+};
