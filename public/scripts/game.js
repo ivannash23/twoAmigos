aaa2be16990ff8eb70734a2d2c48ccfc2eb4f06c
ivannash23 <ivannash23@Gmail.com>
2017-02-19 20:58:28 -0800

    carShip speedValue in place

--- public/scripts/game.js
+++ public/scripts/game.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	
+
 	$.ajax({
 		method: 'GET',
 		url: '/api/score',
@@ -12,10 +12,37 @@ $(document).ready(function(){
 
 	$('#resetGame').on('click', function(){
 		location.reload();
-
 	});
 
+});
 
+
+function onSuccessScore(responseData){
+	var byScore = responseData.slice(0);
+	var highScorePrint = byScore.sort(function(a,b) {return b.score - a.score;});
+	highScorePrint.forEach(function(value, index){
+		$('#scoreList').append(`<li><h4>Name: ${value.name}	Score: ${value.score}</h4></li><hr>`);
+	});
+};
+
+function onSuccessPostScore(responseData){
+	$('#scoreList').append(`<li><h4>Name: ${responseData.name}	Score: ${responseData.score}</h4></li>`);
+};
+
+function pickCarShip(){
+	var carColor = $('#gameCarChoice').val().split(" ");
+	var color = carColor[0].toLowerCase();
+	$('img').attr('src', `/images/Car_${carColor[0]}.png`);
+
+	$.ajax({
+		method: 'GET',
+		url: `/api/carShip/${color}`,
+		success: onSuccessCarShipPick
+	});
+
+};
+
+function onSuccessCarShipPick(responseData){
 	$('#startGame').on('click', function(e){
 
 		var t1= Date.now();
@@ -30,7 +57,7 @@ $(document).ready(function(){
 
 		document.addEventListener("keydown", function(event) {
 	  	if(event.which === charKey){
-	  		marginLeft = marginLeft + 30;
+	  		marginLeft = marginLeft + (10*responseData.speedValue);
 	  		$('#carShip').css({"margin-left": `${marginLeft}px`});
 	  		$('span').empty();
 	  		charKey = Math.round(Math.random() * (90 - 65) + 65);
@@ -42,37 +69,16 @@ $(document).ready(function(){
 	  			var highScore = ( (t2-t1) - score) * -1;
   				var name = window.prompt(`What's your name?`);
   				$.ajax({
-  						method: 'POST',
-  						url: '/api/score',
-  						data: `name=${name}&score=${highScore}` ,
-  						success: onSuccessPostScore
-  					});
+						method: 'POST',
+						url: '/api/score',
+						data: `name=${name}&score=${highScore}` ,
+						success: onSuccessPostScore
+					});
   				$('span').append(`<button class="col-md-2 col-md-offset-5 btn btn-danger"><h2>Game Over</h2></button>`);
 	  		};
-	  	}
+	  	};
+	  });
 	});
-});
-
-
-
-});
-
-function onSuccessScore(responseData){
-	var byScore = responseData.slice(0);
-	var highScorePrint = byScore.sort(function(a,b) {return b.score - a.score;});
-	highScorePrint.forEach(function(value, index){
-		$('#scoreList').append(`<li><h4>Name: ${value.name}	Score: ${value.score}</h4></li><hr>`);
-	});
-};
-
-function onSuccessPostScore(responseData){
-	console.log(responseData);
-	$('#scoreList').append(`<li><h4>Name: ${responseData.name}	Score: ${responseData.score}</h4></li>`);
-};
-
-function pickCarShip(){
-	var carColor = $('#gameCarChoice').val().split(" ");
-	$('img').attr('src', `/images/Car_${carColor[0]}.png`);
 };
 
 
